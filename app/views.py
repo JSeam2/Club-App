0528ef0bc27b3a3f12b404e6e060385c3f165c8c
JSeam2 <joshia_seam@mymail.sutd.edu.sg>
2018-03-24 03:46:43 +0800

    created forms for the logins

--- app/views.py
+++ app/views.py
@@ -1,7 +1,22 @@
-from app import app
 from flask import render_template
+from app import app
+from app.forms import LoginForm, RegisterForm
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     #return app.send_static_file('index.html')
-    return render_template("index.html")
+    login_form = LoginForm()
+    reg_form = RegisterForm()
+
+    #TODO
+    if login_form.validate_on_submit():
+        a = None # login view
+
+    if reg_form.validate_on_submit():
+        a = None # redirect to normal page and prompt user to login
+
+    return render_template("index.html",
+                           login_form = login_form,
+                           reg_form = reg_form)
+
+
